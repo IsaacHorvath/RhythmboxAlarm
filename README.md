6a374ead9df5fbc7fec2c16bbc56090b6f114f45
Isaac Horvath <isaac.horvath@gmail.com>
2013-09-12 01:03:04 -0400

    Finished program, updated README file.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 RhythmboxAlarm
 =============
 
-Allows the user to set a time via commandline argument. Opens rhythmbox once that time is reached.
+Allows the user to set a time via commandline argument. Opens rhythmbox once that time is reached. The process will continue running until that time.
+
+Usage: ./RhythmboxAlarm <time>
+
+E.g. ./RhythmboxAlarm 15:45
--- RhythmboxAlarm.c
+++ RhythmboxAlarm.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
-	printf("Hello, world!");
+int main(int argc, char **argv) {
+	time_t rawtime;
+	struct tm *timeinfo;
+	int hour, minute, currenth, currentm;
+	char execute_string[] = "rhythmbox-client --play";
+	if (argc < 2) {
+		printf("Usage: RhythmboxAlarm <time>\n");
+		return;
+	}
+	sscanf(argv[1], "%d:%d", &hour, &minute);
+	printf("Setting alarm for %02d:%02d\n", hour, minute);
+	do {
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+		currenth = timeinfo->tm_hour;
+		currentm = timeinfo->tm_min;
+		printf("\rThe time is now %02d:%02d.", currenth, currentm);
+		usleep(50000);
+	} while (hour != currenth || minute != currentm);
+	printf("\n");
+	system(execute_string);
 }
